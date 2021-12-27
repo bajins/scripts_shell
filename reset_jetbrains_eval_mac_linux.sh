2c7ef9b1c3aad978652e06d030f046447d7df931
bajins <394585829@qq.com>
2021-12-27 18:50:51 +0800

    update

--- reset_jetbrains_eval_mac_linux.sh
+++ reset_jetbrains_eval_mac_linux.sh
@@ -1,25 +1,27 @@
 #!/bin/sh
-# reset jetbrains ide evals
+# reset jetbrains ide evals for now
 
 OS_NAME=$(uname -s)
 JB_PRODUCTS="IntelliJIdea CLion PhpStorm GoLand PyCharm WebStorm Rider DataGrip RubyMine AppCode"
 
-if [ $OS_NAME == "Darwin" ]; then
+if [ "$OS_NAME" = "Darwin" ]; then
 	echo 'macOS:'
 
 	for PRD in $JB_PRODUCTS; do
-    	rm -rf ~/Library/Preferences/${PRD}*/eval
-    	rm -rf ~/Library/Application\ Support/JetBrains/${PRD}*/eval
+    rm -rf ~/Library/Preferences/${PRD}*/eval
+    rm -rf ~/Library/Application\ Support/${PRD}*/eval
+    rm -rf ~/Library/Application\ Support/JetBrains/${PRD}*/eval
 	done
-elif [ $OS_NAME == "Linux" ]; then
+elif [ "$OS_NAME" = "Linux" ]; then
 	echo 'Linux:'
 
 	for PRD in $JB_PRODUCTS; do
-    	rm -rf ~/.${PRD}*/config/eval
-    	rm -rf ~/.config/${PRD}*/eval
+	  rm -rf ~/.${PRD}*/config/eval
+    rm -rf ~/.config/${PRD}*/eval
+    rm -rf ~/.config/JetBrains/${PRD}*/eval
 	done
 else
-	echo 'unsupport'
+	echo 'unsupported'
 	exit
 fi
 
